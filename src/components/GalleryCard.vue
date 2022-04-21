ac41f8c8a086319d0115a9f6ea315b821b3c13f6
dheeman pati <dheemanpati53@gmail.com>
2022-04-21 16:24:42 +0530

    added animation on loding car cards

--- src/components/GalleryCard.vue
+++ src/components/GalleryCard.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="card shadow-sm mb-3 mt-3">
-    <img v-bind:src="carImage" alt="car-img" class="img-fluid card-img-top" />
-    <div class="card-body">
-      <h5 class="card-title">{{ carName }}</h5>
-      <p class="card-text">{{ carDetails.slice(0, 10) }}...</p>
-      <div class="d-flex justify-content-between mt-3 mb-1">
-        <div>
-          <button class="btn btn-info text-white" @click="handleClick">
-            Info
-          </button>
+  <transition name="list" appear>
+    <div class="card shadow-sm mb-3 mt-2">
+      <img v-bind:src="carImage" alt="car-img" class="img-fluid card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">{{ carName }}</h5>
+        <p class="card-text">{{ carDetails.slice(0, 10) }}...</p>
+        <div class="d-flex justify-content-between mt-3 mb-1">
+          <div>
+            <button class="btn btn-info text-white" @click="handleClick">
+              Info
+            </button>
+          </div>
+          <div class="mt-1">
+            <i
+              @click="editCar(carId)"
+              class="fa-solid fa-lg text-success fa-pen-to-square"
+            ></i>
+            &nbsp;&nbsp;&nbsp;
+            <i
+              @click="deleteCar(carName, carId)"
+              class="fa-solid text-danger fa-lg fa-trash"
+            ></i>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -32,6 +45,8 @@ export default {
     carDetails: String,
     carPrice: String,
     carId: Number,
+    editCar: Function,
+    deleteCar: Function,
   },
 };
 </script>
@@ -50,5 +65,20 @@ img {
 }
 i {
   cursor: pointer;
+}
+
+.list-enter-from {
+  opacity: 0;
+  transform: scale(0.6);
+}
+.list-enter-active {
+  transition: all 0.4s ease;
+}
+.list-leave-to {
+  opacity: 0;
+  transform: scale(0.6);
+}
+.list-leave-active {
+  transition: all 0.6s ease;
 }
 </style>
