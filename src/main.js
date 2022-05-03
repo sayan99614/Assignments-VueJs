27535096e9a6a5d63e027c6530588e35165f5ea3
dheeman pati <dheemanpati53@gmail.com>
2022-05-03 16:08:53 +0530

    added configuration for vueX

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./routes/index";
+import store from "./store/index";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+app.mount("#app");
